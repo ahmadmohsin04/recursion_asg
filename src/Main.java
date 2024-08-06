1f5c988535828237d480a2391ba58c8aebfa4b25
Ahmad Mohsin <business.ahmadmohsin@gmail.com>
2024-08-06 12:17:29 +0500

    q4,5

--- src/Main.java
+++ src/Main.java
@@ -85,7 +85,69 @@
 //        int number = 550;
 //        int digit = 5;
 //        int count = countDigit(number,digit);
-//        System.out.println(count);
+//        System.out.println("The number is : " + number);
+//        System.out.println("The digit to find : " + digit);
+//        System.out.println("The number of digits in that number : " + count);
 //
 //    }
 //}
+
+// Question 4
+
+//We have n people in a room, where n is an integer greater than or equal to 1.
+//Each person shakes hands once with every other person. What is the total number, h(n) ,
+//of handshakes? Write a recursive function to solve this problem.
+//
+//To get you started, if there are only one or two people in the room,
+//then handshake(1) = 0
+//
+//handshake(2) = 1
+//
+//If a third person enters the room, he or she must shake hands with each of
+//the two people already there. This is two handshakes in addition to the number
+//of handshakes that would be made in a room of two people, or a total of three
+//handshakes. If a fourth person enters the room, this is three handshakes in
+//addition to the number of handshakes that would be made in a room of three
+//people handshakes. If you can generalize this to n handshakes, you should be able
+//to write the recursive solution.
+
+//public class Main {
+//    public static int handshake (int n){
+//        if (n==1){
+//            return 0;
+//        } else {
+//            return 1 + handshake(n-1);
+//        }
+//    }
+//    public static void main(String[] args) {
+//
+//        int n = 4;
+//        int hand = handshake(n);
+//        System.out.println("The number of persons : " + n + " persons.");
+//        System.out.println("The number of handshakes : " + hand + " handshakes.");
+//
+//    }
+//}
+
+//Question # 5
+
+//The greatest common divisor of integers x and y is the largest integer that evenly
+//divides both x and y.Write a recursive function GCD that returns the greatest common
+//divisor of x and y?
+
+//public class Main {
+//    public static int GCD (int x , int y){
+//        if (y==0){
+//            return x;
+//        }
+//        return GCD(y ,x%y);
+//    }
+//    public static void main(String[] args) {
+//        int x = 5;
+//        int y = 15;
+//        int gcd = GCD(x,y);
+//        System.out.println(gcd);
+//
+//    }
+//}
+
